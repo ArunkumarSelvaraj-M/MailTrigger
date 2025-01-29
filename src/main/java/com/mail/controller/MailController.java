fed03cb503ccc788e49fe3595034d82695494429
ArunAkilan <arun@cloudgarage.io>
2025-01-29 14:06:35 +0530

    origin updated 2

--- src/main/java/com/mail/controller/MailController.java
+++ src/main/java/com/mail/controller/MailController.java
@@ -17,7 +17,7 @@ import jakarta.mail.MessagingException;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://20.33.20.13:80/", allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class MailController {
 
 	private Map<String, String> response = new HashMap<>();
